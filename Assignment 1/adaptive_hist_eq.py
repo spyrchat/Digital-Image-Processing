d0db104ae452fc16802bc940000e378afca4ac8e
spyrchat <spyrchat@ece.auth.gr>
2024-04-11 16:18:12 +0300

    created adaptive_hist_eq.py

---
+++ Assignment 1/adaptive_hist_eq.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def calculate_eq_transformations_of_regions(img_array: np.ndarray,
+region_len_h: int,region_len_w: int):
+    
+    
--- Assignment 1/demo.py
+++ Assignment 1/demo.py
@@ -3,7 +3,7 @@ import numpy as np
 from global_hist_eq import get_equalization_transform_of_img
 
 
-filename = "/input_img.png"
+filename = "input_img.png"
 img = Image.open(fp=filename)
 bw_img = img.convert("L")
 img_array = np.array(bw_img)
