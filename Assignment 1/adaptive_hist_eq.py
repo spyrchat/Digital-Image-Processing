549a7d0a6e778d2f9363ff1d2b8f49f102433f75
spyrchat <spyrchat@ece.auth.gr>
2024-04-12 09:04:35 +0300

    implemented calculate_eq_transformations_of_regions

--- Assignment 1/adaptive_hist_eq.py
+++ Assignment 1/adaptive_hist_eq.py
@@ -7,12 +7,29 @@ region_len_h: int,region_len_w: int):
     H = img_array.shape[0]
     num_blocks_w = W// region_len_w + (1 if W % region_len_w > 0 else 0)
     num_blocks_h = H // region_len_h + (1 if H % region_len_h > 0 else 0)
-
+    total_blocks = num_blocks_w * num_blocks_h
     top_left_corners = []
-    for i in range(0, W, region_len_w):
-        for j in range(0, H, region_len_h):
-            top_left_corners.append((i, j))
+    region_to_eq_transform = {}
+    # # The outer loop should iterate over the height
+    # for i in range(0, H, region_len_h):
+    #     # The inner loop should iterate over the width
+    #     for j in range(0, W, region_len_w):
+    #         top_left_corners.append((i, j))
 
-    print(top_left_corners)
-    assert len(top_left_corners) == num_blocks_w * num_blocks_h
+    # print(top_left_corners)
+    # assert len(top_left_corners) == total_blocks
 
+    for j in range(num_blocks_h):
+        for i in range(num_blocks_w):
+            top_left_x = i * region_len_w
+            top_left_y = j * region_len_h
+
+            bottom_right_x = min((i+1) * region_len_w, W)
+            bottom_right_y = min((j+1) * region_len_h, H)
+
+            region = img_array[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
+            region_to_eq_transform[(top_left_y,top_left_x)] = get_equalization_transform_of_img(region) 
+
+    return region_to_eq_transform
+
+    
--- Assignment 1/demo.py
+++ Assignment 1/demo.py
@@ -3,11 +3,11 @@ import numpy as np
 from global_hist_eq import get_equalization_transform_of_img
 from adaptive_hist_eq import calculate_eq_transformations_of_regions
 
-filename = "input_img.png"
+filename = "Assignment 1/input_img.png"
 img = Image.open(fp=filename)
 bw_img = img.convert("L")
 img_array = np.array(bw_img)
 
 equalization_transform = get_equalization_transform_of_img(img_array)
-print(equalization_transform)
-calculate_eq_transformations_of_regions(img_array, 64, 48)
+# print(equalization_transform)
+c = calculate_eq_transformations_of_regions(img_array, 48, 64)
