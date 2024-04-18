9df11c79299f3cc01b74a3642d57a1b7e864a855
spyrchat <spyrchat@ece.auth.gr>
2024-04-18 23:04:57 +0300

    added resize option

--- Assignment 1/demo.py
+++ Assignment 1/demo.py
@@ -4,12 +4,16 @@ from global_hist_eq import perform_global_hist_equalization
 from adaptive_hist_eq import perform_adaptive_hist_equalization, perform_adaptive_hist_equalization_no_interpolation
 from helper import plot_histogram, plot_transformation_function, plot_histogram_side_to_side
 
-# Assume calculate_eq_transformations_of_regions and perform_adaptive_hist_equalization
-# are defined in adaptive_hist_eq and imported correctly
-
 # Load the image and convert it to a grayscale NumPy array
 filename = "Assignment 1/input_img.png"
 img = Image.open(filename)
+
+# Resize the image to 512x384 - Optional
+# The image is resized to 512x384 to fit an integer number of 48x64 regions
+# The resampling method used is Lanczos and it will make the image look better when resizing
+# I left this code commented out because the way the algorithms are implemented, they can work with any image size
+# img.resize((512, 384), Image.Resampling.LANCZOS)
+
 bw_img = img.convert("L")
 img_array = np.array(bw_img)
 
@@ -43,5 +47,5 @@ new_img_array_no_interpolation = perform_adaptive_hist_equalization_no_interpola
 new_img_array_no_interpolation = np.clip(new_img_array_no_interpolation, 0, 255).astype(np.uint8)
 new_img_pil_no_interpolation = Image.fromarray(new_img_array_no_interpolation)
 new_img_pil_no_interpolation.save("Assignment 1/adaptive_equalized_image_no_interpolation.png")
-new_img_pil.show()
+new_img_pil_no_interpolation.show()
 plot_histogram(new_img_array_no_interpolation, "Adaptive Equalized Histogram_no_interpolation")
